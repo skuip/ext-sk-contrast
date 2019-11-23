c626e4c0cc095ac04e3dd7f174eb6baca0937fe4
Stefan Kuip <chrome-0d9vd910@skuip.net>
2019-11-24 00:16:33 +0100

    Show contrast values of most used colors

--- src/content_script.js
+++ src/content_script.js
@@ -5,12 +5,13 @@
 	const elements = {
 		aaLarge: null, aaNormal: null, aaaLarge: null, aaaNormal: null, bgHex:
 		null, bgSample: null, canvas: null, contrast: null, fgHex: null,
-		fgSample: null, image: null, selection: null, stats: null
+		fgSample: null, grid: null, image: null, selection: null, stats: null
 	};
 
 	const state = {
 		bgColor: ``,
 		fgColor: ``,
+		luminosity: [],
 		ratio: 0,
 		x1: -1,
 		x2: 0,
@@ -99,6 +100,7 @@
 
 		state.x1 = event.clientX;
 		state.y1 = event.clientY;
+		state.luminosity = [];
 
 		div.shadowRoot.addEventListener(`mousemove`, handleDragMove);
 		div.shadowRoot.addEventListener(`mouseup`, handleDragStop);
@@ -151,15 +153,29 @@
 		}
 
 		// Convert from object to array
-		let list = Object.keys(colors).map(key => ({ color: +key, count: colors[key] }));
-
-		const threshold = Math.floor(width * height / 1000);
-		if (threshold >= 1) {
-			list = list.filter(item => item.count > threshold);
-		}
+		const pixels = width * height / 100;
+		let list = Object.keys(colors).map(key => ({
+			color: +key,
+			count: colors[key],
+			percentage: colors[key] / pixels
+		}));
 
 		// Calculate luminosity for each color
-		const luminosity = list.map(item => ({ ...item, luminosity: calculateLuminosity(item.color) }));
+		const luminosity = list.map(item => ({
+			hex: rgb2hex(item.color).toUpperCase(),
+			luminosity: calculateLuminosity(item.color),
+			...item
+		}));
+
+		luminosity.sort((a, b) => b.count - a.count);
+
+		for (let i = 0; i < luminosity.length - 1; i++) {
+			if (luminosity[i].disabled) continue;
+			for (let j = i + 1; j < luminosity.length; j++) {
+				const d = distance(luminosity[i].color, luminosity[j].color);
+				if (d < 0.1) luminosity[j].disabled = true;
+			}
+		}
 
 		// Order array on luminosity
 		luminosity.sort((a, b) => a.luminosity - b.luminosity);
@@ -174,9 +190,10 @@
 		const foreground = min.count > max.count ? max : min;
 
 		// Store measurement
-		state.bgColor = `#` + rgb2hex(background.color);
-		state.fgColor = `#` + rgb2hex(foreground.color);
+		state.bgColor = `#` + rgb2hex(background.color).toUpperCase();
+		state.fgColor = `#` + rgb2hex(foreground.color).toUpperCase();
 		state.ratio = calculateRatio(min.luminosity, max.luminosity);
+		state.luminosity = luminosity;
 
 		render();
 	}
@@ -184,15 +201,14 @@
 	function render() {
 		const {
 			aaLarge, aaNormal, aaaLarge, aaaNormal, bgHex, bgSample,
-			contrast, fgHex, fgSample, selection, stats
+			contrast, fgHex, fgSample, grid, selection, stats
 		} = elements;
 
-		const { bgColor, fgColor, ratio, x1, x2, y1, y2, zoom } = state;
+		const { bgColor, fgColor, luminosity, ratio, x1, x2, y1, y2, zoom } = state;
 
 		// Position selection window
 		const style = selection.style;
 		selection.classList.toggle(`hidden`, x1 < 0);
-		console.log(selection.className);
 		if (x1 >= 0) {
 			const left   = Math.min(x1, x2);
 			const top    = Math.min(y1, y2);
@@ -208,18 +224,91 @@
 
 		// Fill in the stats on measurement
 		stats.classList.toggle(`is-visible`, ratio > 0);
-		if (ratio > 0) {
-			stats.style.zoom = 1 / zoom;
-			aaLarge.classList.toggle(`pass`, ratio>3.0);
-			aaNormal.classList.toggle(`pass`, ratio>4.5);
-			aaaLarge.classList.toggle(`pass`, ratio>4.5);
-			aaaNormal.classList.toggle(`pass`, ratio>7.0);
-			bgHex.innerText = bgColor;
-			bgSample.style.background = bgColor;
-			contrast.innerText = ratio > 10 ? ratio.toFixed(4) : ratio.toFixed(5);
-			fgHex.innerHTML = fgColor;
-			fgSample.style.background = fgColor;
+
+		if (ratio <= 0) return;
+
+		stats.style.zoom = 1 / zoom;
+		aaLarge.classList.toggle(`pass`, ratio>3.0);
+		aaNormal.classList.toggle(`pass`, ratio>4.5);
+		aaaLarge.classList.toggle(`pass`, ratio>4.5);
+		aaaNormal.classList.toggle(`pass`, ratio>7.0);
+		bgHex.innerText = bgColor;
+		bgSample.style.background = bgColor;
+		contrast.innerText = ratio > 10 ? ratio.toFixed(4) : ratio.toFixed(5);
+		fgHex.innerHTML = fgColor;
+		fgSample.style.background = fgColor;
+
+		while (grid.firstElementChild) {
+			grid.removeChild(grid.firstElementChild);
 		}
+
+		const colors = luminosity.filter(a => !a.disabled);
+		colors.sort((a, b) => b.count - a.count);
+		colors.splice(10);
+
+		console.log(luminosity);
+		console.log(colors);
+
+		let hexes = `<tr><th></th>`
+		let percs = `<tr><th></th>`
+		for (let ix = 0; ix < colors.length; ix++) {
+			const lx = colors[ix];
+
+			let th = `<th class="`;
+			if ( calculateRatio(lx.luminosity, 0.05) < calculateRatio(lx.luminosity, 1.05)) {
+				th += `white`;
+			} else {
+				th += `black`;
+			}
+			th += `" style="background:#` + rgb2hex(lx.color) + `">`;
+
+			hexes += th + `<span>` + rgb2hex(lx.color).toUpperCase() + `</span></th>`;
+			percs += th + lx.percentage.toFixed(2).substr(0, 4) + `%</th>`;
+		}
+		hexes += `</tr>`;
+		percs += `</tr>`;
+
+		let html = hexes + percs;
+
+		for (let iy = 1; iy < colors.length; iy++) {
+			const ly = colors[iy];
+
+			html += `<tr><th class="`
+			if ( calculateRatio(ly.luminosity, 0.05) < calculateRatio(ly.luminosity, 1.05)) {
+				html += `white`;
+			} else {
+				html += `black`;
+			}
+			html += `" style="background:#` + rgb2hex(ly.color) + `">` + rgb2hex(ly.color).toUpperCase();
+			html += `</th>`;
+
+			for (let ix = 0; ix < colors.length; ix++) {
+				const lx = colors[ix];
+
+				const ratio = calculateRatio(lx.luminosity, ly.luminosity);
+
+				if (iy > ix) {
+					html += `<td style="background:`;
+					if (ratio < 3.0) {
+						html += `#D00`;
+					}
+					else if (ratio < 4.5) {
+						html += `#A50`;
+					}
+					else {
+						html += `#070`;
+					}
+					// Don't round up 4.4999 to 4.5, it is just not the same
+					html += `">` + (Math.floor(ratio * 100) / 100).toFixed(2) + `</td>`
+				} else {
+					html += `<td></td>`;
+				}
+			}
+
+			html += `</tr>`;
+		}
+
+		grid.innerHTML = html;
 	}
 
 	function calculateRatio(l1, l2) {
@@ -252,6 +341,15 @@
 
 		let luminosity = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 		return luminosity + 0.05;
+	}
+
+	// https://github.com/gdkraus/wcag2-color-contrast
+	function distance(rgb1, rgb2) {
+		let r = (((rgb1 >> 16) & 255) / 255) - (((rgb2 >> 16) & 255) / 255);
+		let g = (((rgb1 >>  8) & 255) / 255) - (((rgb2 >>  8) & 255) / 255);
+		let b = (((rgb1 >>  0) & 255) / 255) - (((rgb2 >>  0) & 255) / 255);
+
+		return Math.sqrt(r * r + g * g + b * b);
 	}
 
 	/**
